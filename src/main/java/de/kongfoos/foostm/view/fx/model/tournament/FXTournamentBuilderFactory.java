066bbc2666a04690d82de3d329245d8a4bfe35d9
raQai <patrick.bgxn@gmail.com>
2016-07-28 20:54:01 +0200

    (Tournament) Apply recent model changes

--- src/main/java/de/kongfoos/foostm/view/fx/model/tournament/FXTournamentBuilderFactory.java
+++ src/main/java/de/kongfoos/foostm/view/fx/model/tournament/FXTournamentBuilderFactory.java
@@ -1,14 +1,14 @@
 package de.kongfoos.foostm.view.fx.model.tournament;
 
 
-import de.kongfoos.foostm.model.tournament.TournamentBuilder;
+import de.kongfoos.foostm.model.tournament.ATournamentBuilder;
 
 public class FXTournamentBuilderFactory {
     public static Builder create() {
         return new Builder();
     }
 
-    public static class Builder extends TournamentBuilder<FXTournament> {
+    public static class Builder extends ATournamentBuilder<FXTournament> {
         @Override
         protected FXTournament getIntance() {
             return new FXTournament();
