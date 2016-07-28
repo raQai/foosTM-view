1fb55f1ce479ecbd21923f601ce6a037b913aeca
raQai <patrick.bgxn@gmail.com>
2016-07-28 20:55:23 +0200

    Apply recent model changes to PlayerUtils

--- src/main/java/de/kongfoos/foostm/view/PlayerUtils.java
+++ src/main/java/de/kongfoos/foostm/view/PlayerUtils.java
@@ -1,20 +1,20 @@
 package de.kongfoos.foostm.view;
 
 import com.google.common.base.Strings;
-import de.kongfoos.foostm.model.player.PlayerImpl;
+import de.kongfoos.foostm.model.player.APlayer;
 
 public class PlayerUtils {
     private static final String UNSPECIFIED = "<unspecified>";
 
-    public static String printWithDTFB(PlayerImpl player) {
+    public static String printWithDTFB(APlayer player) {
         return printWithID(player, player.getDtfb());
     }
 
-    public static String printWithITSF(PlayerImpl player) {
+    public static String printWithITSF(APlayer player) {
         return printWithID(player, player.getItsf());
     }
 
-    private static String printWithID(PlayerImpl player, String id) {
+    private static String printWithID(APlayer player, String id) {
         return player.toString() + " (" + (!Strings.isNullOrEmpty(id) ? id : UNSPECIFIED) + ")";
     }
 }
